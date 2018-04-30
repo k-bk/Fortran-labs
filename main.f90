eaed4f81f9e90bf3cd296e43283f162326b8153a
Karol Bąk <bak.karolek@gmail.com>
2018-04-30 13:11:52 +0200

    Add tridiagonal_solver() and module test

---
+++ main.f90
@@ -0,0 +1,7 @@
+program main
+    use matrix
+    implicit none
+    write(*,*) "Hello world"
+    call helloMatrix ()
+end program main
+
--- matrix.f90
+++ matrix.f90
@@ -1,6 +1,47 @@
-program main
+module matrix
+    implicit none
+contains
+
+! ---- tridiagonal_solve ---------------------------
+! Solves a tridiagonal system using simplified Gauss
+! elimination algorithm.
+!
+!   [A] x [X] = [D]
+!
+subroutine tridiagonal_solve (A,X,D,n) 
     implicit none
 
-    write(*,*) "Hello world"
+    real (kind=8), intent(inout) :: A(:,:), D(:)
+    real (kind=8), intent(out) :: X(:)
+    real (kind=8) :: multi
+    integer (kind=8) :: i,n
 
-end program main
+    n = size(D)
+    A(1,3) = A(1,3) / A(1,2)
+    D(1) = D(1) / A(1,2)
+
+    do i = 2,n
+        multi = A(i-1,2) / A(i,1) 
+        A(i,1) = 0
+        A(i,2) = A(i,2) * multi - A(i-1,3) 
+        D(i) = D(i) * multi - D(i-1)
+        D(i) = D(i) / A(i,2)
+        A(i,:) = A(i,:) / A(i,2)
+    end do
+
+    ! Obtaining result usig back substitution
+    X(n) = D(n)
+    do i = n-1,0,-1
+        X(i) = D(i) - A(i,3) * X(i+1)
+    end do
+end subroutine
+
+! ---- hello_matrix --------------------------------
+! Function used to test module linking.
+!
+subroutine hello_matrix ()
+    write(*,*) "Welcome to the matrix"
+end subroutine
+
+end module matrix
+
